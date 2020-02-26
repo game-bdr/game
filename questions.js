e4845a0e22599074fe64ae9e632739511dd77c0a
mah-far <mah.mf89@gmail.com>
2020-02-26 15:09:07 +0100

    final design

--- questions.js
+++ questions.js
@@ -1,39 +1,4 @@
- var questions = [
-  {
-    "name": "Gorebyss",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Gorebyss_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "His body is durable enough to withstand high-pressure levels in the deep sea. Do not confuse with HBase, Couchbase, MeshBase, Graphbase or KirbyBase.",
-    "img": "gorebyss.png"
-  },
-  {
-    "name": "Tokutek",
-    "url": "https://www.percona.com/",
-    "type": 1,
-    "text": "Tokutek claims to improve MongoDB performance 20x. It is unclear if this also means losing 20x as many documents.",
-    "img": "tokutek.png"
-  },
-  {
-    "name": "Adabas",
-    "url": "https://en.wikipedia.org/wiki/ADABAS",
-    "type": 1,
-    "text": "ADABAS was NoSQL from a time when there was no SQL. The technology now is owned by Software AG. \"Software AG: We're not sure what we do either.\"",
-    "img": "adabas.gif"
-  },
-  {
-    "name": "Feebas",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Feebas_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Feebas requires Swift Swim to get marvel scale.",
-    "img": "feebas.png"
-  },
-  {
-    "name": "Azurill",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Azurill_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Azurill's tail is large and bouncy. Azurill can be seen bouncing and playing on its big, rubbery tail. On sunny days they gather at the edge of water and splash about for fun.",
-    "img": "azurill.png"
-  },
+ let questions = [
   {
     "name": "Hadoop",
     "url": "https://hadoop.apache.org/",
@@ -69,146 +34,65 @@
     "text": "This Twitter technology combines streaming, MapReduce, Scalding, Scala and Storm. Potential users must prove they are a hipster before they are allowed to download.",
     "img": "summingbird.png"
   },
-  {
-    "name": "Akiban",
-    "url": "http://www.akiban.com/",
-    "type": 1,
-    "text": "Touted as SQL database with object structured storage, Akiban was recently acquired by another database vendor you have never heard of.",
-    "img": "akiban.jpg"
-  },
-  {
-    "name": "Arbok",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Arbok_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Despite reports to the contrary, Arbok does not evolve into Seviper.",
-    "img": "arbok.png"
-  },
-  {
-    "name": "Flink",
-    "url": "https://flink.apache.org/",
-    "type": 1,
-    "text": "Apache Flink will help us with batch and stream processing, but the logo is cute enough to become Pokémon.",
-    "img": "flink.png"
-  },
-  {
-    "name": "Impala",
-    "url": "http://impala.io/",
-    "type": 1,
-    "text": "Querying big data is too slow? Impala has a solution for it. But reconsider taming that one, it is built on C++.",
-    "img": "impala.png"
-  },
-  {
-    "name": "Pangool",
-    "url": "http://pangool.net/",
-    "type": 1,
-    "text": "It's like Sandlash, but it will help us to develop map reduce jobs for Hadoop.",
-    "img": "pangool.png"
-  },
-  {
-    "name": "Azkaban",
-    "url": "http://azkaban.github.io/",
-    "type": 1,
-    "text": "It's not the evolution from Abra, Kadabra, or Alakazam, nor related with Harry Potter. Azkaban is workflow scheduler, just like crontab, but with graphical interface.",
-    "img": "azkaban.png"
-  },
-  {
-    "name": "Arvados",
-    "url": "https://arvados.org/",
-    "type": 1,
-    "text": "Not to be confused with Ariados. Spins a web of microservices around unsuspecting sysadmins.",
-    "img": "arvados.png"
-  },
-  {
-    "name": "Seahorse",
-    "url": "https://seahorse.deepsense.io/",
-    "type": 1,
-    "text": "Not to be confused with Horsea. Seahorse lets you create Apache Spark applications in a visual way.",
-    "img": "seahorse.png"
-  },
-  {
-    "name": "Horsea",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Horsea_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Horsea is a small, blue, seahorse-like Pokémon with a single dorsal fin and a tightly curled tail.",
-    "img": "horsea.png"
-  },
-  {
-    "name": "Titan",
-    "url": "http://thinkaurelius.github.io/titan/",
-    "type": 1,
-    "text": "Titan is a distributed graph database that is unfortunately not related to the legendary titans, a term coined by Pokémon the Movie 2000 - The Power of One, referring to Articuno, Zapdos, and Moltres.",
-    "img": "titan.png"
-  },
-  {
-    "name": "Atlassian",
-    "url": "https://www.atlassian.com/",
-    "type": 1,
-    "text": "Atlassian has somehow duped several software companies into thinking they need JIRA to be agile; if only we all had the agility move in real life.",
-    "img": "atlassian.png"
-  },
-  {
-    "name": "Jirachi",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Jirachi_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Jirachi is a steel and psychic-type Pokémon with no known evolutions. It should not be confused with JIRA, an issue tracker for software teams.",
-    "img": "jirachi.png"
-  },
-  {
-    "name": "Spoink",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Spoink_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Spoink is a bouncy psychic-type Pokémon that should not be confused with Splunk, a software suite for analyzing log data.",
-    "img": "spoink.png"
-  },
-  {
-    "name": "Debezium",
-    "url": "https://debezium.io",
-    "type": 1,
-    "text": "Debezium is a tool for turning your coherent relational data into indecipherable big data. Not to be confused with Meganium the herb Pokémon",
-    "img": "debezium.jpg"
-  },
-  {
-    "name": "Geode",
-    "url": "https://geode.apache.org/",
-    "type": 1,
-    "text": "Geode is a database like model, which makes people to ask more why Hadoop is not RDBMS. Not to be confused with Geodude which rocks",
-    "img": "geode.png"
-  },
-  {
-    "name": "Geodude",
-    "url": "http://bulbapedia.bulbagarden.net/wiki/Geodude_(Pok%C3%A9mon)",
-    "type": 0,
-    "text": "Geodude is a hard rock pokemon immune to one hit ko, not like my Hadoop cluster. Unlike Geode which make you more confused about big data.",
-    "img": "geodude.jpg"
-  },
-  {
-    "name": "Ambari",
-    "url": "https://ambari.apache.org/",
-    "type": 1,
-    "text": "Ambari is a tool that makes administration of Hadoop easy so you can make fun of the developers when they run out of executors. Not to be confused with Ambipom which is a long tail pokemon.",
-    "img": "ambari.png"
-  }
 ];
 
+let result = 0;
+let qNum = 0;
+let itemSelected = [];
+let i;
 
-var max = questions.length;
-var min = 0;
-var result = 0;
-var qNum = 0;
+function selectNum() {
+    i = Math.floor(Math.random() * questions.length);
+    if(!itemSelected.includes(i) && itemSelected.length < questions.length){
+        itemSelected.push(i);
+        return true;
+    }
+    if(itemSelected.length >= questions.length) {
+        return false;
+    }
+    selectNum();
+    return true;
+}
 
 function startGame() {
-	var i = Math.floor(Math.random() * (max - min + 1) ) + min;
-	document.getElementById('demo').innerHTML = '<h3>is it Big data or Pokemon?</h3><br/><h1>'+questions[i].name+'</h1><br/><br/><button class="btn btn-info btn-lg" tag="pokemon" onclick="checkAnswer(0,'+i+','+questions[i].type+')">Pokemon</button><span style="margin:0 10%"></span><button class="btn btn-info btn-lg" tag="bigdata" onclick="checkAnswer(1,'+i+','+questions[i].type+')">Big Data</button>';
+    let res = selectNum();
+    if(res) {
+        document.getElementById('demo').innerHTML = '<h1>'+questions[i].name+'</h1><br/>' +
+            '<p>Is it <b>BigData Technology</b> or <b>Roche Product</b>?</p><br/>' +
+            '<div class="row justify-content-around mb-3">' +
+            '<button class="btn btn-info btn-md" tag="pokemon" onclick="checkAnswer(0,'+i+','+questions[i].type+')">Roche Product</button>' +
+            '<button class="btn btn-info btn-md" tag="bigdata" onclick="checkAnswer(1,'+i+','+questions[i].type+')">BigData Technology</button>' +
+            '</div>';
+    }
+    else {
+        showResult();
+    }
 }
 function checkAnswer(x, i, p){
 	qNum ++;
+	let next = 'Next Question';
+	let finish = 'See your Result';
 	if (p === x) {
 		result ++;
 	}
-	document.getElementById('demo').innerHTML = '<h3>'+questions[i].text+'</h3><img src="img/'+questions[i].img+'" style="float:right; margin-right:10%; max-width:300px; max-height:300px" /><div style="clear:both"></div><br/><br/><button class="btn btn-info btn-lg" onclick="showResult();">Results</button><span style="margin:0 10%"></span><button class="btn btn-success btn-lg" onclick="startGame()">Next</button>'
+	let button =
+        qNum === (questions.length) ?
+            '<button class="btn btn-info btn-md mb-3" onclick="showResult();">' + finish + '</button>' :
+            '<div class="row justify-content-around mb-3">' +
+            '<button class="btn btn-info btn-md mb-3" onclick="showResult();">' + finish + '</button>' +
+            '<button class="btn btn-success btn-md mb-3" onclick="startGame()">' + next +'</button>' +
+            '</div>';
+
+	let type = questions[i].type ? 'Roche Product' : 'BigData Technology';
+	document.getElementById('demo').innerHTML =
+        '<h3><b>' + questions[i].name +'</b> is <br/><b>' + type +'</b></h3><br/>' +
+        '<img src="img/'+questions[i].img+'" style="max-width:250px; max-height:250px"/>' +
+        '<p class="description">'+questions[i].text+'</p><br/>' + button;
 }
 function showResult(){
-	document.getElementById('demo').innerHTML = '<h1>Thank you for playing our game</h1><br/><h3>You answered '+result+' correct question(s) from '+qNum+' question(s):</h3><br/><br/><p>your score is: '+parseInt((result/qNum)*100)+' from 100.</p><br/><br/><button class="btn btn-success btn-lg" onclick="location.reload(); ">Main page</button>'
+	document.getElementById('demo').innerHTML =
+        '<h2><b>Thank you for playing the game</b></h2><br/>' +
+        '<p><b>You answered '+result+' correct question(s) from '+qNum+' question(s):</b></p><br/>' +
+        '<p style="font-size: 18px"><b>your score is: '+parseInt((result/qNum)*100)+' from 100</b></p><br/><br/>' +
+        '<button class="btn btn-success btn-lg" onclick="location.reload(); ">Play again!</button>'
 }
-
-//document.getElementById('demo').innerHTML = '<h1>'+questions[i].text+'</h1>';
